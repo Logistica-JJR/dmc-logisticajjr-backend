e1d1e563f0db860fbccad18b4b5c0488dc4fef59
unknown <ricvera@favoritafc.com>
2026-04-05 13:24:46 -0500

    Add package for H2

--- src/test/java/com/logisticajjr/bodega/BodegaApplicationTests.java
+++ src/test/java/com/logisticajjr/bodega/BodegaApplicationTests.java
@@ -2,6 +2,7 @@ package com.logisticajjr.bodega;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
